9cd9a909a15fc1a495adfa4293cd2337b231ca8b
MatthewSchuette <mjschuette4@gmail.com>
2020-04-24 11:35:48 -0500

    Updated ColorList but cant login

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, update }) => {
   console.log(colors);
+  const { push } = useHistory();
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,10 +23,25 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res.data);
+        update();
+        setEditing(false);
+        push("/bubblepage");
+      })
+      .catch(err => console.log("colorList put error -", err));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/api/colors/${color.id}`)
+      .then(res => {
+        update();
+      })
+      .catch(err => console.log(err));
   };
 
   return (
