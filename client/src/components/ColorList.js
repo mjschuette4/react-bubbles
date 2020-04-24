dca11a912f3f4b48e330a4d449316b2d8d7c896d
MatthewSchuette <mjschuette4@gmail.com>
2020-04-24 13:08:45 -0500

    fixed minor oops

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -18,7 +18,7 @@ const ColorList = ({ colors, updateColors, update }) => {
   };
 
   const saveEdit = e => {
-    e.preventDefault();
+    // e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -19,7 +19,7 @@ const Login = props => {
   const handleSubmit = e => {
     e.preventDefault();
     axiosWithAuth()
-    .post("http://localhost:5000/api/login", credentials)
+    .post("/api/login", credentials)
     .then(res => {
       console.log(res);
       localStorage.setItem('token', res.data.payload);
